78d868e6e0197dbf2a64577627a30c2604cfa4bb
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2014-09-06 18:01:02 +0100

    Add boilerplate class code

---
+++ php-arraypaginate.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title:         PHP ArrayPaginate
+ * Version:       0.1
+ * Description:   Paginate PHP arrays with markup for navigation
+ * Author:        Lawrence Okoth-Odida
+ * Documentation: https://github.com/lokothodida/php-arraypaginate/wiki/
+ */
+class ArrayPaginate {
+  /** constants*/
+
+  /** properties */
+
+  /** public methods */
+
+?>
