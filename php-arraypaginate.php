38ec4c7a91d06bfe0e05bfb11db305939e88be10
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2014-09-07 14:16:04 +0100

    Flesh out navigation builder

--- php-arraypaginate.php
+++ php-arraypaginate.php
@@ -91,11 +91,46 @@ class ArrayPaginate {
     $itemsPerPage  = $this->options['itemsPerPage'];
     $start         = ($currentPage - 1) * $itemsPerPage;
     $this->results = array_slice($this->items, $start, $itemsPerPage);
+    $this->totalPages = ceil(count($this->items) / $itemsPerPage);
   }
 
   // Build up the navigation html
   private function buildNavigation() {
+    $html        = '';
+    $currentPage = $this->options['currentPage'];
+    $totalPages  = $this->totalPages;
+    $labels      = $this->options['labels'];
+
+    // first
+    $html .= $this->createNavigationAnchor(1, $labels['first']);
+    // prev
+
+    $html .= $this->createNavigationAnchor($currentPage - 1, $labels['prev']);
+
+    // numbers
     // ...
+
+    // next
+    $html .= $this->createNavigationAnchor($currentPage + 1, $labels['next']);
+
+    // last
+    $html .= $this->createNavigationAnchor($totalPages, $labels['last']);
+
+    $this->navigation = $html;
+  }
+
+  // Create a navigation link
+  private function createNavigationAnchor($pageNumber, $label = false) {
+    $html = '';
+
+    // set the label to the page number if it isn't supplied
+    if (!$label) {
+      $label = $pageNumber;
+    }
+
+    // ...
+
+    return $html;
   }
 }
 
